029b789c52256fff237040aa5fe73fd1ae9bf5a0
Karjalainen Kim TTV15SP <kimkarjalainen@kamk.fi>
2016-04-15 13:48:53 +0300

    Git gud fgts

--- Pip/Pip/Enemy.h
+++ Pip/Pip/Enemy.h
@@ -1,31 +1,33 @@
-//#ifndef ENEMY_H
-//#define ENEMY_H
-//
-//#include <SFML/Graphics.hpp>
-//#include "Projectile.h"
-//
-//using namespace sf;
-//
-//class Enemy
-//{
-//public:
-//	CircleShape enemyShape;
-//
-//	FloatRect getEnemyBox();
-//
-//	Projectile* projectile;
-//
-//	float enemySize = 5.0;
-//	int enemySpeed = 2500;
-//	int enemyHP = 100;
-//
-//	Enemy();
-//	~Enemy();
-//
-//	void setStartPosition();
-//	Vector2f getPosition();
-//
-//	void Update(Clock& clock);
-//	void Draw(RenderWindow& window);
-//};
-//#endif
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <SFML/Graphics.hpp>
+#include "Projectile.h"
+
+using namespace sf;
+
+class Enemy
+{
+public:
+	CircleShape enemyShape;
+
+	FloatRect getEnemyBox();
+
+	Projectile* projectile;
+
+	std::vector<Enemy*> enemies;
+
+	float enemySize = 5.0;
+	int enemySpeed = 2500;
+	int enemyHP = 100;
+
+	Enemy();
+	~Enemy();
+
+	void setStartPosition();
+	Vector2f getPosition();
+
+	//void Update(Clock& clock);
+	void Draw(RenderWindow& window);
+};
+#endif
