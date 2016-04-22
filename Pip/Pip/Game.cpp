4d1b0a51f4ead58464f6b3bdfafc304707ca20c1
Karjalainen Kim TTV15SP <kimkarjalainen@kamk.fi>
2016-04-22 14:01:58 +0300

    Spoon status: RAPED

--- Pip/Pip/Game.cpp
+++ Pip/Pip/Game.cpp
@@ -104,6 +104,7 @@ void Game::Update()
 				std::cout << "Enemy " << enemies.size() << "\n";
 				std::cout << "Score: " << ++score << "\n";
 			}
+			//Commit: spoon status raped
 
 			//Checks if player collides with enemy and gives damage to player
 			if (player->GetPlayerBoundingBox().intersects(enemy->GetEnemyBoundingBox()))
